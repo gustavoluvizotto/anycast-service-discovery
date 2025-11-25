ff7b776fdd71ec5c6e36480ceceedb404ddf6ce4
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-11-25 12:19:08 +0100

    downloading anycast census

--- census_helper.py
+++ census_helper.py
@@ -4,6 +4,8 @@
 import pandas as pd
 from datetime import datetime
 import requests
+import argparse
+
 
 def download_date(year, month, day, version) -> pd.DataFrame:
     """Download and return census data for a specific date as a DataFrame.
@@ -29,7 +31,7 @@ def download_date(year, month, day, version) -> pd.DataFrame:
     date_str = f"{year:04d}/{month:02d}/{day:02d}"
     url = f"https://github.com/ut-dacs/anycast-census/blob/main/{date_str}/IP{version}.parquet?raw=true"
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=60)
     if response.status_code != 200:
         raise Exception(f"Failed to download {url}: HTTP {response.status_code}")
 
@@ -38,3 +40,21 @@ def download_date(year, month, day, version) -> pd.DataFrame:
     # Read the Parquet data into a DataFrame
     df = pd.read_parquet(pd.io.common.BytesIO(data_bytes))
     return df
+
+
+def main(args):
+    datetime_obj = datetime.strptime(args.date, "%Y%m%d")
+    pdf = download_date(datetime_obj.year, datetime_obj.month, datetime_obj.day, args.version)
+    output_path = args.output_dir if args.output_dir else "."
+    pdf.to_csv(f"{output_path}/anycast_census_{datetime_obj.year}_{datetime_obj.month:02d}_{datetime_obj.day:02d}_{args.version}.csv", index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--version", required=True, type=str,
+                        help="v4 or v6")
+    parser.add_argument("--date", required=True, type=str,
+                        help="Download single snapshot. Format: YYYYMMDD")
+    parser.add_argument("--output-dir", required=False, type=str,
+                        help="Where to place the csv file. Default: current directory")
+    main(parser.parse_args())

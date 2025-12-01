fc8dbdf361d533a4ae994fb924bd5847c01f1f12
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-12-01 17:23:01 +0100

    zgrab script improv.

--- measurements/run_zgrab.sh
+++ measurements/run_zgrab.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <input_file> <protocol_version=v4|v6>"
+    exit 1
+fi
+
+mkdir -p results/zgrab
+
+INPUT_FILE=$(basename $1)
+PROTOCOL_VERSION=$2
+TIMESTAMP=$(TZ=":UTC" date '+%Y%m%d%H%M%S')
+TIME_OUTPUT="zgrab_time_${TIMESTAMP}.txt"
+
+if [[ "${PROTOCOL_VERSION}" == "v4" ]]; then
+    EXTRA_PARAMS="--dns-resolver=127.0.0.1:5335"
+elif [[ "${PROTOCOL_VERSION}" == "v6" ]]; then
+    EXTRA_PARAMS="--resolve-ipv6 --dns-resolver=[::1]:5335"  # TODO check v6 resolver
+fi
+
 time { \
-    docker compose run --rm zgrab2 -f "input/zgrab/${INPUT_FILE}" -o "results/zgrab/zgrab_${PORT}_${TIMESTAMP}_${PROTOCOL_VERSION}.csv"
+    docker compose run --rm zgrab2 banner -f "input/zgrab/${INPUT_FILE}" -o "results/zgrab/zgrab_${TIMESTAMP}_${PROTOCOL_VERSION}.csv" #${EXTRA_PARAMS}
 } 2> results/zgrab/"${TIME_OUTPUT}"

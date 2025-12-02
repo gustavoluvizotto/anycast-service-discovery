e21069ac2b24bcf08fae6c7016d1a59699ead644
Johannes Zirngibl <johannes.zirngibl@mpi-inf.mpg.de>
2025-12-02 15:15:33 +0100

    alllow different alpn values

--- measurements/run_quic_scan.sh
+++ measurements/run_quic_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <input_file> <protocol_version=v4|v6>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <input_file> <protocol_version=v4|v6> <alpn>"
     exit 1
 fi
 
@@ -9,16 +9,23 @@ mkdir -p results/quic
 
 INPUT_FILE=$1
 PROTOCOL_VERSION=$2
+ALPN=$3
 
 # Rate limit based on leaky bucket
 BUCKET_REFILL_DURATION=2
 BUCKET_SIZE=500
 
 TIMESTAMP=$(TZ=":UTC" date '+%Y%m%d%H%M%S')
-OUTPUT_DIR="results/quic/quic_${PROTOCOL_VERSION}_${TIMESTAMP}"
+OUTPUT_DIR="results/quic/quic_${PROTOCOL_VERSION}_${ALPN}_${TIMESTAMP}"
+
+if [[ "${ALPN}" == "h3" ]]; then
+    ALPN_ARGUMENT="-alpn h3 -http3"
+else
+    ALPN_ARGUMENT="-alpn ${ALPN}"
+fi
 
 sysctl -w net.core.rmem_max=7500000
 sysctl -w net.core.wmem_max=7500000
 
-docker compose run --rm quic -keylog -output $OUTPUT_DIR -input $INPUT_FILE -bucket-refill-duration $BUCKET_REFILL_DURATION -bucket-size $BUCKET_SIZE 
+docker compose run --rm quic -keylog -output $OUTPUT_DIR -input $INPUT_FILE -bucket-refill-duration $BUCKET_REFILL_DURATION -bucket-size $BUCKET_SIZE ${ALPN_ARGUMENT}
 RETURNCODE=$?

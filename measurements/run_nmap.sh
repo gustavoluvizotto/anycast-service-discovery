1d05e70f8a04edb18d5638edf08e6beb3bd556a6
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-12-02 10:31:28 +0100

    update nmap script and port selection input for zmap

--- measurements/run_nmap.sh
+++ measurements/run_nmap.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <input_file> <protocol_version=v4|v6>"
+if [ $# -lt 2 ] || [ $# -gt 4 ]; then
+    echo "Usage: $0 <input_file> <protocol_version=v4|v6> [shard] [nshard]"
     exit 1
 fi
 
+SHARD=""
+NSHARD=""
+SHARD_TXT=""
+if [ -z "$3" ] && [ -z "$4" ]; then
+    echo "Sharding not enabled"
+else
+    echo "Sharding enabled: shard=$3 nshard=$4"
+    SHARD=$3  # optional
+    NSHARD=$4  # optional
+    SHARD_TXT="_${SHARD}_of_${NSHARD}_"
+fi
 
 mkdir -p results/nmap
 
 INPUT_FILE=$(basename $1)
 PROTOCOL_VERSION=$2
 TIMESTAMP=$(TZ=":UTC" date '+%Y%m%d%H%M%S')
-TIME_OUTPUT="nmap_time_${TIMESTAMP}.txt"
+TIME_OUTPUT="nmap_time_${TIMESTAMP}${SHARD_TXT}.txt"
 
 if [[ "${PROTOCOL_VERSION}" == "v6" ]]; then
     echo "Scanning IPv6 addresses"
@@ -22,11 +33,6 @@ fi
 
 { time \
     # https://nmap.org/book/man-performance.html
-    #  -T4 prohibits the dynamic scan delay from exceeding 10 ms for TCP ports
-    # --host-timeout: maximum amount of time you are willing to wait on a single host (default of T5 is 15m)
-    # A host that times out is skipped. No port table, OS detection, or version detection results are printed for that host.
-
     # https://www.siberoloji.com/adjusting-parallelism-min-parallelism-max-parallelism-with-nmap/
-    # --min-parallelism: set the minimum number of parallel probes
-    docker compose run --rm nmap -T4 ${PROTOCOL_VERSION} --script='' --max-retries 3 --min-parallelism 40 --host-timeout 2m -oX "results/nmap/nmap_${TIMESTAMP}_${PROTOCOL_VERSION}.xml" -iL "input/nmap/${INPUT_FILE}" -n -sS
+    docker compose run --rm nmap -T4 ${PROTOCOL_VERSION} --script='' --max-retries 3 --min-parallelism 40 --host-timeout 2m --top-ports 2000 -oX "results/nmap/nmap_${TIMESTAMP}_${PROTOCOL_VERSION}${SHARD_TXT}.xml" -iL "input/nmap/${INPUT_FILE}" -n -sS
 } 2> results/nmap/"${TIME_OUTPUT}"

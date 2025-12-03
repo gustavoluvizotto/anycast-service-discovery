12c9c6c65aae3affe8a15df895463fc526d39b9d
Gustavo - sinteustatius scanner <g.luvizottocesar@utwente.nl>
2025-12-03 10:49:29 +0000

    fix dns resolver flag

--- measurements/run_zgrab.sh
+++ measurements/run_zgrab.sh
@@ -13,11 +13,11 @@ TIMESTAMP=$(TZ=":UTC" date '+%Y%m%d%H%M%S')
 TIME_OUTPUT="zgrab_time_${TIMESTAMP}.txt"
 
 if [[ "${PROTOCOL_VERSION}" == "v4" ]]; then
-    EXTRA_PARAMS="--dns-resolver=127.0.0.1:5335"
+    EXTRA_PARAMS="--dns-resolvers=127.0.0.1:5335"
 elif [[ "${PROTOCOL_VERSION}" == "v6" ]]; then
-    EXTRA_PARAMS="--resolve-ipv6 --dns-resolver=[::1]:5335"  # TODO check v6 resolver
+    EXTRA_PARAMS="--resolve-ipv6 --dns-resolvers=[::1]:5335"  # TODO check v6 resolver
 fi
 
 time { \
-    docker compose run --rm zgrab banner -f "input/zgrab/${INPUT_FILE}" -o "results/zgrab/zgrab_${TIMESTAMP}_${PROTOCOL_VERSION}.csv" #${EXTRA_PARAMS}
+    docker compose run --rm zgrab multiple -c input/zgrab/zgrab_config.ini -f "input/zgrab/${INPUT_FILE}" -o "results/zgrab/zgrab_${TIMESTAMP}_${PROTOCOL_VERSION}.csv" ${EXTRA_PARAMS}
 } 2> results/zgrab/"${TIME_OUTPUT}"

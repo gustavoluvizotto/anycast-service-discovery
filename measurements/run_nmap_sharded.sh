12210c72bbf0341b43eae567eaf2da048757e845
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-12-04 17:07:52 +0100

    fix run nmap sharded

--- measurements/run_nmap_sharded.sh
+++ measurements/run_nmap_sharded.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <shard_dir> <protocol_version=v4|v6>"
+    exit 1
+fi
+
 SHARD_DIR=$1
+PROTOCOL_VERSION=$2
 
 for f in ${SHARD_DIR}/*.csv; do
     base=$(basename "$f")
@@ -11,6 +17,7 @@ for f in ${SHARD_DIR}/*.csv; do
     total="${base#*_of_}"           # remove up to "_of_"
     total="${total%%_*}"            # keep everything before next "_"
 
-    ./run_nmap.sh $f v4 $shard_num $total
+    ./run_nmap.sh $f $PROTOCOL_VERSION $shard_num $total &
 done
 
+wait

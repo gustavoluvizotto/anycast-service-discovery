c0c5b46e4337981d7de2279213dd74d635b8ea1b
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-12-14 17:53:40 +0100

    hrp zgrap input

--- measurements/upload_zgrab_data.sh
+++ measurements/upload_zgrab_data.sh
@@ -5,9 +5,9 @@ mc cp results/zgrab/zgrab_20251214090624_v4_full.log zgrab-write/catrin/artefact
 
 mc cp results/zgrab/zgrab_time_20251214090624.txt zgrab-write/catrin/artefacts/tool=zgrab/dataset=tcp-anycast/vp=nl-ens/port=top100/year=2025/month=12/day=14/zgrab_time_20251214090624.txt
 
-#mc cp input/zgrab/zgrab_input_100ports_tcp-anycast_v4.csv zgrab-write/catrin/artefacts/tool=zgrab/dataset=tcp-anycast/vp=nl-ens/port=top100/year=2025/month=12/day=14/zgrab_input_100ports_tcp-anycast_v4.csv
+mc cp input/zgrab/zgrab_input_100ports_tcp-anycast_v4.csv zgrab-write/catrin/artefacts/tool=zgrab/dataset=tcp-anycast/vp=nl-ens/port=top100/year=2025/month=12/day=14/zgrab_input_100ports_tcp-anycast_v4.csv
 
-#mc cp input/zgrab/zgrab_config.ini zgrab-write/catrin/artefacts/tool=zgrab/dataset=tcp-anycast/vp=nl-ens/port=top100/year=2025/month=12/day=14/zgrab_config.ini
+mc cp input/zgrab/zgrab_config.ini zgrab-write/catrin/artefacts/tool=zgrab/dataset=tcp-anycast/vp=nl-ens/port=top100/year=2025/month=12/day=14/zgrab_config.ini
 
 # measurements
-#mc cp results/zgrab/zgrab_20251214090624_v4.jsonl zgrab-write/catrin/measurements/tool=zgrab/dataset=tcp-anycast/vp=nl-ens/port=top100/year=2025/month=12/day=14/zgrab_20251214090624_v4.jsonl
+mc cp results/zgrab/zgrab_20251214090624_v4.jsonl zgrab-write/catrin/measurements/tool=zgrab/dataset=tcp-anycast/vp=nl-ens/port=top100/year=2025/month=12/day=14/zgrab_20251214090624_v4.jsonl

dc8a056d20fade440f724ff2cada217936929cd8
Gustavo - sinteustatius scanner <g.luvizottocesar@utwente.nl>
2025-12-14 08:49:44 +0000

    fix zgrab time logging

--- measurements/run_zgrab.sh
+++ measurements/run_zgrab.sh
@@ -19,8 +19,7 @@ elif [[ "${PROTOCOL_VERSION}" == "v6" ]]; then
 fi
 
 # -s is the number of goroutines to use :--help
-# --target-timeout (60s default) is the overall timeout for scanning a single host :--deepwiki
-# --connect-timeout (10s default) is specifically for establishing the initial connection
-#time { \
+# --read-limit-per-host is the amount of kb to read back from hosts
+{ time  \
     docker compose run --rm zgrab multiple -c input/zgrab/zgrab_config.ini -f "input/zgrab/${INPUT_FILE}" -o "results/zgrab/zgrab_${TIMESTAMP}_${PROTOCOL_VERSION}.jsonl" ${EXTRA_PARAMS} -s 5000 --read-limit-per-host=2 &> results/zgrab/zgrab_${TIMESTAMP}_${PROTOCOL_VERSION}_full.log;
-#} &> results/zgrab/"${TIME_OUTPUT}"
+} &> results/zgrab/"${TIME_OUTPUT}"

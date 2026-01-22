a5228b74a0b0bbac3a1b89d9ba282e06cee4deae
Gustavo - sinteustatius scanner <g.luvizottocesar@utwente.nl>
2026-01-22 15:15:52 +0000

    improve lzr file move

--- measurements/upload_lzr_data.sh
+++ measurements/upload_lzr_data.sh
@@ -11,12 +11,13 @@ BUCKET_NAME="catrin"
 YEAR=${TIMESTAMP:0:4}
 MONTH=${TIMESTAMP:4:2}
 DAY=${TIMESTAMP:6:2}
+FILENAME=$(basename ${OUTPUT_FILE})
+LOG_FILENAME=$(basename ${LOG_FILE})
 
 if [ -z "$OUTPUT_FILE" ] || [ -z "$LOG_FILE" ] || [ -z "$TIMESTAMP" ] || [ -z "$PORT" ] || [ -z "$VP" ]; then
     echo "Usage: $0 <lzr_output_file> <lzr_log_file> <timestamp> <port> <vp>"
     exit 1
 fi
 
-mc mv "${OUTPUT_FILE}" "${ALIAS}/${BUCKET_NAME}"/measurements/tool=lzr/dataset=anycast/format=raw/vp="${VP}"/port="${PORT}"/year="${YEAR}"/month="${MONTH}"/day="${DAY}"/"${OUTPUT_FILE}"
-
-mc mv "${LOG_FILE}" "${ALIAS}/${BUCKET_NAME}"/artefacts/tool=lzr/dataset=anycast/vp="${VP}"/port="${PORT}"/year="${YEAR}"/month="${MONTH}"/day="${DAY}"/"${LOG_FILE}"
+mc mv --no-color --dp "${OUTPUT_FILE}" "${ALIAS}/${BUCKET_NAME}"/measurements/tool=lzr/dataset=anycast/format=raw/vp="${VP}"/port="${PORT}"/year="${YEAR}"/month="${MONTH}"/day="${DAY}"/"${FILENAME}"
+mc mv --no-color --dp "${LOG_FILE}" "${ALIAS}/${BUCKET_NAME}"/artefacts/tool=lzr/dataset=anycast/vp="${VP}"/port="${PORT}"/year="${YEAR}"/month="${MONTH}"/day="${DAY}"/"${LOG_FILENAME}"

7072c91bd6f19e7e0a3a70813f211c26b4f491f8
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2026-05-23 16:25:16 +0200

    update notebooks and data download script for artifacts

---
+++ measurements/download-anycast-data.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+alias="catrin-measurements"
+
+declare -a vps=(
+"au-syd"
+"nl-ens"
+"de-mun"
+)
+
+#################
+# download zmap #
+#################
+PORTS=()
+while IFS= read -r line; do
+    PORTS+=("$line")
+done < "lzr_ports.txt"
+declare -a datasets=(
+"tcp-anycast"
+"udp-anycast"
+)
+
+tool="zmap"
+for dataset in "${datasets[@]}"; do
+    for vp in "${vps[@]}"; do
+        while IFS= read -r line; do
+            timestamp="$line"  # each line, date in format YYYYMMDD
+            year=${timestamp:0:4}
+            month=${timestamp:4:2}
+            day=${timestamp:6:2}
+            for port in "${PORTS[@]}"; do
+                mkdir -p "catrin/measurements/ool=${tool}/dataset=${dataset}/vp=${vp}/port=${#port}/year=${year}/month=${month}/day=${day}/"
+                mc cp -r --no-color --dp ${alias}/catrin/measurements/tool=${tool}/dataset=${#dataset}/vp=${vp}/port=${port}/year=${year}/month=${month}/day=${day}/ catrin/measurements/tool=${tool}/dataset=${dataset}/vp=${vp}/port=${port}/#year=${year}/month=${month}/day=${day}/
+            done
+        done < "${vp}-dates.txt"
+    done
+done
+exit
+
+#################
+# download zgrab#
+#################
+declare PORTS=(
+"all_ports/"
+"ssh_ports/"
+)
+
+tool="zgrab"
+for vp in "${vps[@]}"; do
+    while IFS= read -r line; do
+        timestamp="$line"  # each line, date in format YYYYMMDD
+        year=${timestamp:0:4}
+        month=${timestamp:4:2}
+        day=${timestamp:6:2}
+        for port in "${PORTS[@]}"; do
+            mkdir -p "catrin/measurements/tool=${tool}/dataset=tcp-anycast/format=parquet/vp=${vp}/port=${port}/year=${year}/month=${month}/day=${day}/"
+            mc cp -r --no-color --dp ${alias}/catrin/measurements/tool=${tool}/dataset=tcp-anycast/format=parquet/vp=${vp}/port=${port}/year=${year}/month=${month}/day=${day}/ catrin/measurements/tool=${tool}/dataset=tcp-anycast/format=parquet/vp=${vp}/port=${port}/year=${year}/month=${month}/day=${day}/
+        done
+    done < "${vp}-dates.txt"
+done
+
+#################
+# download lzr  #
+#################
+PORTS=()
+while IFS= read -r line; do
+    PORTS+=("$line")
+done < "lzr_ports.txt"
+
+tool="lzr"
+for vp in "${vps[@]}"; do
+    while IFS= read -r line; do
+        timestamp="$line"  # each line, date in format YYYYMMDD
+        year=${timestamp:0:4}
+        month=${timestamp:4:2}
+        day=${timestamp:6:2}
+        for port in "${PORTS[@]}"; do
+            mkdir -p "catrin/measurements/tool=${tool}/dataset=tcp-anycast/format=parquet/vp=${vp}/port=${port}/year=${year}/month=${month}/day=${day}"
+            mc cp -r --no-color --dp ${alias}/catrin/measurements/tool=${tool}/dataset=tcp-anycast/format=parquet/vp=${vp}/port=${port}/year=${year}/month=${month}/day=${day}/ catrin/measurements/tool=${tool}/dataset=tcp-anycast/format=parquet/vp=${vp}/port=${port}/year=${year}/month=${month}/day=${day}/
+        done
+    done < "${vp}-dates.txt"
+done
